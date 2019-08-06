d1482e839de539dd7a0c3a6d31da2fd6a1eb8fff
abdulrauf84 <abdul.rauf@live.se>
2019-08-06 02:46:55 +0200

    javascript code started

---
+++ week-2/main.js
@@ -0,0 +1,37 @@
+console.log('Hello World! // English');
+console.log('Hej Världen! // Swedish');
+console.log('Hej Verden! // Danish');
+console.log('Hello Dunya! //Urdu');
+
+//2
+console.log("I'm awesome");
+
+//3
+let x;
+console.log("The value of my variable x will be: undefined");
+console.log(x);
+ x = 1;
+console.log("The value of my variable x will be: 1");
+console.log(x);
+
+//4
+let y = "Hello";
+console.log("The value of my variable y will be: Hello");
+console.log(y);
+y = "Hello World";
+console.log("The value of my variable y will be: Hello World");
+console.log(y);
+
+
+//5
+let z = 7.25;
+console.log(z);
+let a = Math.round(z);
+console.log(a);
+let b;
+if(z > a){
+    b = z
+}else{
+    b = a
+}
+console.log(b);
